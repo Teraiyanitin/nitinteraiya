063c0bd5ae6aad6631c78db4c4f3a2aabc48df2a
Teraiyanitin <165872523+Teraiyanitin@users.noreply.github.com>
2024-04-19 18:11:54 +0530

    46

---
+++ 46.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "abc123";
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+mysqli_close($conn);
+?>
